794183d045b39aca02fa516f1c6b5fe64fecf568
Jeffrey Chidi <jeffdev1111@gmail.com>
2018-07-18 17:47:06 +0100

    Added code to stop users from having two carts

--- FoodMax/urls.py
+++ FoodMax/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('add/', views.add, name='add_to_cart'),
     path('remove/', views.remove, name='remove_from_cart'),
+    path('new_cart/', views.newCart, name='new_cart'),
     path('email/', views.email_order, name='email_order'),
     path('add_quantity/', views.add_quantity, name='add_quantity'),
     path('minus_quantity/', views.minus_quantity, name='minus_quantity'),
--- FoodMax/views.py
+++ FoodMax/views.py
@@ -43,21 +43,51 @@ def restaurants_detail(request, pk):
     cart = Cart(request.session)
     location = request.GET.get('ln')
     restaurant = get_object_or_404(Restaurant, pk=pk)
+    restaurant_selected_cart = request.session.get('restaurant_cart')
+    if (pk == restaurant_selected_cart):
+        check = True
+    else:
+        check = False
+    restaurant_selected_cart_detail = Restaurant.objects.filter(id=restaurant_selected_cart)
     coverageDetail = CoverageDetail.objects.filter(restaurant__id=pk).filter(coverage_id=location)
     price = cart.total
     return render(request, 'foodMax/restaurants_detail.html', context={
         'restaurant': restaurant,
         'coverageDetail': coverageDetail,
         'cart': cart,
-        'price': price
+        'price': price,
+        'check':check,
+        'restaurant_selected_cart': restaurant_selected_cart,
+        'restaurant_selected_cart_detail': restaurant_selected_cart_detail,
     })
 
 
 def add(request):
-    cart = Cart(request.session)
-    product = Submenu.objects.get(id=request.GET.get('dish_id'))
-    cart.add(product, price=product.price)
-    return HttpResponse("Added")
+    if request.session.get('restaurant_cart') == None:
+        response = HttpResponse({"error": "there was an error"})
+        response.status_code = 403  # To announce that the user isn't allowed to publish
+        return response
+
+    if request.session.get('restaurant_cart'):
+        restaurant_selected_cart = request.session.get('restaurant_cart')
+        # restaurant_selected_cart = request.session.get('restaurant_cart', 0)
+        restaurant = request.GET.get('dr')
+        if (restaurant == restaurant_selected_cart):
+            cart = Cart(request.session)
+            product = Submenu.objects.get(id=request.GET.get('dish_id'))
+            cart.add(product, price=product.price, )
+            return HttpResponse("Added")
+        else:
+            response = HttpResponse({"error": "there was an error"})
+            response.status_code = 403  # To announce that the user isn't allowed to publish
+            return response
+    else:
+        restaurant = request.GET.get('dr')
+        cart = Cart(request.session)
+        restaurant_selected_cart = request.session.get('restaurant_cart', restaurant)
+        product = Submenu.objects.get(id=request.GET.get('dish_id'))
+        cart.add(product, price=product.price, )
+        return HttpResponse("Added")
 
 
 def remove(request):
@@ -92,5 +122,19 @@ def email_order(request):
     send_mail('New Order', json.dumps(cart), 'foodmax@jaytechx.com', [restaurant.email, ])
     return HttpResponse("Email sent")
 
+
 def checkout(request):
-    return render(request,'foodMax/checkout.html')
+    restaurant_selected_cart = request.session.get('restaurant_cart')
+    restaurant = Restaurant.objects.filter(id=restaurant_selected_cart)
+    return render(request, 'foodMax/checkout.html', context={
+        'restaurant_selected_cart': restaurant_selected_cart,
+        'restaurant': restaurant
+    })
+
+
+def newCart(request):
+    restaurant = request.GET.get('dr')
+    cart = Cart(request.session)
+    request.session['restaurant_cart'] = restaurant
+    cart.clear()
+    return HttpResponse("New cart created")
